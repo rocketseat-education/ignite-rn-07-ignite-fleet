d5cc3cf2e5b31b4cfe6e572368e5b587dc62794c
RennanD <rplaykl@gmail.com>
2023-04-18 15:16:41 -0300

    Estruruta do projeto - Adicionando o Styled Components

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme';
+
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   return (
-    <SignIn />
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
   );
 }
 
