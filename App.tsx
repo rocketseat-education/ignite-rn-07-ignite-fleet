45e30c87d7a3496916d4631fa13a1644ad7f7746
RennanD <rplaykl@gmail.com>
2023-04-18 14:46:29 -0300

    Estruruta do projeto - Criando projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SignIn />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
