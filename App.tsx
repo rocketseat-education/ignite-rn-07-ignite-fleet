a9a7210a2c9ec088e03388daf596f04a25fd4222
RennanD <rplaykl@gmail.com>
2023-04-25 17:54:16 -0300

    Navegação & Safe Area Context - useSafeAreaInsets

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components/native';
 import { StatusBar } from 'react-native';
 import { AppProvider, UserProvider } from '@realm/react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
@@ -27,15 +28,17 @@ export default function App() {
 
   return (
     <AppProvider id={REALM_APP_ID}>
-        <ThemeProvider theme={theme}>
-        <StatusBar 
-          barStyle="light-content" 
-          backgroundColor="transparent" 
-          translucent 
-        />
-        <UserProvider fallback={SignIn}>
-          <Routes />
-        </UserProvider>
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider>
+          <StatusBar 
+            barStyle="light-content" 
+            backgroundColor="transparent" 
+            translucent 
+          />
+          <UserProvider fallback={SignIn}>
+            <Routes />
+          </UserProvider>
+        </SafeAreaProvider>
       </ThemeProvider>
     </AppProvider>
     
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,7 +1,7 @@
 import { TouchableOpacity } from 'react-native'
 import { Power } from 'phosphor-react-native'
-
 import { useUser, useApp } from '@realm/react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { Container, Greeting, Message, Name, Picture } from './styles';
 
@@ -10,13 +10,16 @@ import theme from '../../theme';
 export function HomeHeader() {
   const user = useUser();
   const app = useApp();
+  const insets = useSafeAreaInsets();
+
+  const paddingTop = insets.top + 32;
 
   function handleLogOut() {
     app.currentUser?.logOut();
   }
 
   return (
-    <Container>
+    <Container style={{ paddingTop }}>
       <Picture 
         source={{ uri: user?.profile.pictureUrl }}
         placeholder='L184i9ofbHof00ayjsay~qj[ayj@'
