47e5b0bd90bb8d5056a4f933ab9fad681ae29aca
RennanD <rplaykl@gmail.com>
2023-04-25 17:49:07 -0300

    Navegação & Safe Area Context - Tipagem das rotas

--- App.tsx
+++ App.tsx
@@ -20,7 +20,7 @@ export default function App() {
   })
 
   if(!fontsLoaded) {
-    return (
+    return ( 
       <Loading />
     )
   }
---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,7 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined;
+    }
+  }
+}
