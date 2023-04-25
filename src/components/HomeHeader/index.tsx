99154d190a860f2fd65ba603d071b201c487720a
RennanD <rplaykl@gmail.com>
2023-04-25 17:33:58 -0300

    Cloud Mongodb Atlas - Logout

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,7 +1,7 @@
 import { TouchableOpacity } from 'react-native'
 import { Power } from 'phosphor-react-native'
 
-import { useUser } from '@realm/react';
+import { useUser, useApp } from '@realm/react';
 
 import { Container, Greeting, Message, Name, Picture } from './styles';
 
@@ -9,6 +9,11 @@ import theme from '../../theme';
 
 export function HomeHeader() {
   const user = useUser();
+  const app = useApp();
+
+  function handleLogOut() {
+    app.currentUser?.logOut();
+  }
 
   return (
     <Container>
@@ -26,7 +31,7 @@ export function HomeHeader() {
         </Name>
       </Greeting>
 
-      <TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogOut}>
         <Power size={32} color={theme.COLORS.GRAY_400} />
       </TouchableOpacity>
     </Container>
