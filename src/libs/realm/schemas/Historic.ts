47702fd0196b4b489890ae7927902ce29a74c56b
RennanD <rplaykl@gmail.com>
2023-05-09 12:04:16 -0300

    Implementando o RealmDB - Método generate

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,8 +1,30 @@
 import { Realm } from '@realm/react';
 
-export class Historic extends Realm.Object<Historic> {
-  static generate() {
+type GenerateProps = {
+  user_id: string;
+  description: string;
+  license_plate: string
+}
 
+export class Historic extends Realm.Object<Historic> {
+  _id!: string
+  user_id!: string
+  license_plate!: string;
+  description!: string;
+  status!: string
+  created_at!: string;
+  updated_at!: string;
+
+  static generate({ user_id,description,license_plate }: GenerateProps) {
+    return {
+      _id: new Realm.BSON.UUID(),
+      user_id,
+      description,
+      license_plate,
+      status: 'departure',
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
   }
 
   static schema = {
