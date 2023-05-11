a02b497cfdfab319fd59790d5c36e7ae4dba276e
RennanD <rplaykl@gmail.com>
2023-05-11 11:52:12 -0300

    Banco de Dados local - Criando a tela de registrar chegada

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,6 +1,9 @@
 import { useRoute } from '@react-navigation/native';
 
-import { Container } from './styles';
+import { Container, Content, Description, Footer, Label, LicensePlate } from './styles';
+
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
 
 type RouteParamProps = {
   id: string;
@@ -13,11 +16,30 @@ export function Arrival() {
   const { id } = route.params as RouteParamProps;
 
 
-  console.log(id);
-
   return (
     <Container>
+      <Header title='Chegada' />
+      <Content>
+        <Label>
+          Placa do veículo
+        </Label>
 
+        <LicensePlate>
+          XXX0000
+        </LicensePlate>
+
+        <Label>
+          Finalidade
+        </Label>
+
+        <Description>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa voluptate atque necessitatibus voluptatibus eveniet rerum maiores neque laborum obcaecati eos debitis deleniti tempore veritatis, voluptates modi, optio ullam quasi dolor!
+        </Description>
+
+        <Footer>
+          <Button title='Registrar chegada' />
+        </Footer>
+      </Content>
     </Container>
   );
 }
