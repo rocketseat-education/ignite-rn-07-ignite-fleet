39a1c08dd8570959bf086d4e7e709822d84c45d9
RennanD <rplaykl@gmail.com>
2023-06-28 18:26:17 -0300

    Mapa - Finalizando o Mapa

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -137,10 +137,7 @@ export function Departure() {
 
       <KeyboardAwareScrollView extraHeight={100}>
         <ScrollView>
-          { currentCoords && <Map coordinates={[
-            { latitude: -5.0792, longitude: -42.7895 },
-            {latitude: -5.0815, longitude: -42.7792}
-          ]} /> }
+          { currentCoords && <Map coordinates={[currentCoords]} /> }
 
           <Content>
             {
