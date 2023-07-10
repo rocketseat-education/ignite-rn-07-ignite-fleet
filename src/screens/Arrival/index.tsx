1c0b3a64df3bdb6e48fd11d17cf4f330f22b4aa3
RennanD <rplaykl@gmail.com>
2023-07-10 16:36:19 -0300

    Finalizando o App - Registrada a chegada

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -64,9 +64,12 @@ export function Arrival() {
         return Alert.alert('Erro', 'Não foi possível obter os dados para registrar a chegada do veículo.')
       }
 
+      const locations = await getStorageLocations()
+
       realm.write(() => {
         historic.status = 'arrival';
         historic.updated_at = new Date();
+        historic.coords.push(...locations)
       });
 
       await stopLocationTask()
