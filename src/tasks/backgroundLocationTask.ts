5fd1b59550beffb720cd5e28b44f7a0203183916
RennanD <rplaykl@gmail.com>
2023-07-10 15:27:05 -0300

    Background Task - Utilizando o saveStorageLocation

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -5,27 +5,31 @@ import {
   stopLocationUpdatesAsync
 } from 'expo-location'
 import * as TaskManager from 'expo-task-manager';
+import { saveStorageLocation } from '../libs/asyncStorage/locationStorage';
 
 export const BACKGROUND_TASK_NAME =  'location-tracking';
 
-TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async ({ data, error }: any) => {
   try {
     if(error) {
       throw error
     }
 
-    const { coords, timestamp } = data.locations[0];
+    if(data) {
+      const { coords, timestamp } = data.locations[0];
 
-    const currentLocation = {
-      latitude: coords.latitude,
-      longitude: coords.longitude,
-      timestamp
+      const currentLocation = {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        timestamp
+      }
+
+      await saveStorageLocation(currentLocation)
     }
-
-    console.log(currentLocation)
 
   } catch (error) {
     console.log(error)
+    stopLocationTask();
   }
 })
 
