560782d950f7d6b309102b06a06c17f97ef12e96
RennanD <rplaykl@gmail.com>
2023-07-10 16:40:45 -0300

    Finalizando o App - Obtendo as coordenadas do banco

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -92,8 +92,14 @@ export function Arrival() {
     const updatedAt= historic!.updated_at.getTime(); 
     setDataNotSynced(updatedAt > lastSync);
 
-    const locationsStorage = await getStorageLocations();
-    setCoordinates(locationsStorage)
+    if(historic?.status === 'departure') {
+      const locationsStorage = await getStorageLocations();
+      setCoordinates(locationsStorage);
+    } else {
+      setCoordinates(historic?.coords ?? []);
+    }
+
+    
   }
 
   useEffect(() => {
