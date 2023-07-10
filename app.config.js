52e17ad2db16bfbb20300e43c30420ffc3e8c184
RennanD <rplaykl@gmail.com>
2023-07-10 11:24:12 -0300

    Background Task - Configurando as permissões

--- app.config.js
+++ app.config.js
@@ -24,6 +24,9 @@ module.exports = {
       "bundleIdentifier": "com.rennand.ignitefleet",
       "config": {
         "googleMapsApiKey": process.env.GOOGLE_MAPS_API_KEY
+      },
+      "infoPlist": {
+        "UIBackgroundModes": ["location"]
       }
     },
     "android": {
@@ -32,6 +35,11 @@ module.exports = {
         "backgroundColor": "#202024"
       },
       "package": "com.rennand.ignitefleet",
+      "permissions": [
+        "ACCESS_FINE_LOCATION",
+        "ACCESS_COARSE_LOCATION",
+        "ACCESS_BACKGROUND_LOCATION"
+      ],
       "config": {
         "googleMaps": {
           "apiKey": process.env.GOOGLE_MAPS_API_KEY
