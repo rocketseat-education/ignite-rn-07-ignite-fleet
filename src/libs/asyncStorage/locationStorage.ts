b3c387c0497b364723f4a14b9b0a496fd1623318
RennanD <rplaykl@gmail.com>
2023-07-10 15:24:57 -0300

    Background Task - Removendo as coordenadas no storage

--- src/libs/asyncStorage/locationStorage.ts
+++ src/libs/asyncStorage/locationStorage.ts
@@ -8,7 +8,7 @@ type LocationProps = {
   timestamp: number;
 }
 
-export async function getStorageLocation() {
+export async function getStorageLocations() {
   const storage = await AsyncStorage.getItem(STORAGE_KEY)
 
   const response = storage ? JSON.parse(storage) : []
@@ -17,9 +17,13 @@ export async function getStorageLocation() {
 }
 
 export async function saveStorageLocation(newLocation: LocationProps) {
-  const storage = await getStorageLocation()
+  const storage = await getStorageLocations()
 
   storage.push(newLocation)
 
   await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(storage))
+}
+
+export async function removeStorageLocations() {
+  await AsyncStorage.removeItem(STORAGE_KEY)
 }
