6c32eb0a8276dfd3f424362577e916aaedcc0939
RennanD <rplaykl@gmail.com>
2023-07-10 12:14:12 -0300

    Background Task - Criando a função para iniciar a task

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -1,3 +1,4 @@
+import { Accuracy, startLocationUpdatesAsync } from 'expo-location'
 import * as TaskManager from 'expo-task-manager';
 
 export const BACKGROUND_TASK_NAME =  'location-tracking';
@@ -22,3 +23,15 @@ TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
     console.log(error)
   }
 })
+
+export async function startLocationTask() {
+  try {
+    await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
+      accuracy: Accuracy.Highest,
+      distanceInterval: 1,
+      timeInterval: 1000
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
