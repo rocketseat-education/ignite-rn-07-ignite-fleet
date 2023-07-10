fc59238454808db0be12809c8968ba8a37327e36
RennanD <rplaykl@gmail.com>
2023-07-10 15:20:14 -0300

    Background Task - Salvando as coordenadas no storage

--- src/libs/asyncStorage/locationStorage.ts
+++ src/libs/asyncStorage/locationStorage.ts
@@ -2,10 +2,24 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const STORAGE_KEY = '@ignitefleet:location';
 
+type LocationProps = {
+  latitude: number;
+  longitude: number;
+  timestamp: number;
+}
+
 export async function getStorageLocation() {
   const storage = await AsyncStorage.getItem(STORAGE_KEY)
 
   const response = storage ? JSON.parse(storage) : []
 
   return response
+}
+
+export async function saveStorageLocation(newLocation: LocationProps) {
+  const storage = await getStorageLocation()
+
+  storage.push(newLocation)
+
+  await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(storage))
 }
